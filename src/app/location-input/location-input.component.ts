7245d190d51e555e83708d98679d574f67d906c4
Darin <darindev1000@gmail.com>
2019-01-26 06:41:22 -0800

    Added working map link

--- src/app/location-input/location-input.component.ts
+++ src/app/location-input/location-input.component.ts
@@ -22,6 +22,10 @@ export class LocationInputComponent implements OnInit {
   entertainment = false;
   parks = false;
 
+  businessData = {};
+
+  mapLink = '#';
+  mapName = '';
 
   constructor() { }
 
@@ -46,10 +50,46 @@ export class LocationInputComponent implements OnInit {
         categories: categories
       };
       console.log(body);
-      axios.post(`${environment.apiURL}/getGeographicCenter`, body);
-      console.log("sent");
+      this.businessData = axios.post(`${environment.apiURL}/getGeographicCenter`, body)
+        .then( (res) => { 
+          console.log(res.data);
+          this.generateMapLink(res.data);
+        } );
+      // console.log(this.businessData);
       // this.rest.post(`${environment.apiURL}/submitAddress`, body);
     }
+  }
+
+  generateMapLink(businessData) {
+    console.log('works');
+    const alias = businessData.alias;
+    const name = businessData.name;
+    const coordinates = businessData.coordinates;
+    const rating = businessData.rating;
+
+    const googlePlace = axios.get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyDTR1tSdwSPeBMEPc_pmhDvTyzNstDly8g`)
+      .then((googlePlace) => {
+        console.log(googlePlace);
+        this.displayMapLink(googlePlace, businessData);
+      } );
+  }
+
+  displayMapLink(googlePlace, businessData) {
+    const alias = businessData.alias;
+    const name = businessData.name;
+    const coordinates = businessData.coordinates;
+    const rating = businessData.rating;
+
+    const regex = /-/g;
+    const aliasModified = alias.replace(regex, '+');
+
+    const mapURL = `https://www.google.com/maps/search/?api=1&query=${aliasModified}&query=${coordinates.latitude},${coordinates.longitude}`;
+
+    // const mapURL = `https://www.google.com/maps/place/${alias}/@${coordinates.latitude},${coordinates.longitude},14z`;
+    console.log(mapURL);
+
+    this.mapLink = mapURL;
+    this.mapName = 'Route to Meeting Location';
   }
 
 }
