9fccbe1face4f9e50197695e86c3c3e781b05c57
Darin <darindev1000@gmail.com>
2019-01-26 17:25:59 -0800

    Get map route showing

--- src/app/location-input/location-input.component.ts
+++ src/app/location-input/location-input.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { RestService } from "../../services/rest.service";
 import axios from "axios";
 import { environment } from '../../environments/environment';
+import { coreDirectives } from '@agm/core/core.module';
 
 @Component({
   selector: 'app-location-input',
@@ -10,11 +11,12 @@ import { environment } from '../../environments/environment';
   styleUrls: ['./location-input.component.scss']
 })
 export class LocationInputComponent implements OnInit {
-  myAddress = '5200 Lake Rd';
-  myZip = '95340';
-  theirAddress = '233 2nd St NW';
-  theirZip = '20001';
+  myAddress = '4701 Stoddard Rd';
+  myZip = '95356';
+  theirAddress = '18405 Mt Hamilton Rd';
+  theirZip = '95140';
   // rest = new RestService;
+  show = false;
 
   lat: Number = 37.799448;
   lng: Number = -120.979021;
@@ -23,6 +25,7 @@ export class LocationInputComponent implements OnInit {
   public origin: any;
   public destination: any;
 
+  meetingLocationName;
 
   coffee = false;
   restaurant = false;
@@ -38,12 +41,14 @@ export class LocationInputComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.getDirection();
+    // this.getDirection();
   }
 
-  getDirection() {
-    this.origin = { lat: 37.799448, lng: -120.979021 };
-    this.destination = { lat: 37.799524, lng: -120.975017 };
+  getDirection(origin, destination) {
+    // this.origin = { lat: 37.799448, lng: -120.979021 };
+    // this.destination = { lat: 37.799524, lng: -120.975017 };
+    this.origin = origin;
+    this.destination = destination;
   }
 
   submitAddress() {
@@ -63,14 +68,33 @@ export class LocationInputComponent implements OnInit {
         theirZip: this.theirZip,
         categories: categories
       };
+
       console.log(body);
       this.businessData = axios.post(`${environment.apiURL}/getRouteCenter`, body)
         .then( (res) => { 
           console.log(res.data);
           this.displayMapLink(res.data);
         } );
+
+        // axios.post(`${environment.apiURL}/submitAddress`, { address: this.myAddress } )
+        // .then( (res) => { console.log("responste", res.data) });
+
       // console.log(this.businessData);
-      // this.rest.post(`${environment.apiURL}/submitAddress`, body);
+       axios.post(`${environment.apiURL}/submitAddress`, { address: this.myAddress })
+        .then( (origin) => {
+          this.origin  = origin.data;
+          console.log('orig', origin.data);
+          axios.post(`${environment.apiURL}/submitAddress`, { address: this.theirAddress })
+            .then( (destination) => {
+              console.log('dest', destination.data);
+              this.destination = destination.data;
+              console.log(this.origin.data, this.destination.data);
+
+              // Set map route
+              // this.getDirection(this.origin, this.destination)
+              // this.show = true;
+            });
+        });
     }
   }
 
@@ -91,6 +115,7 @@ export class LocationInputComponent implements OnInit {
   displayMapLink(businessData) {
     const alias = businessData.alias;
     const name = businessData.name;
+    this.meetingLocationName = name;
     const coordinates = businessData.coordinates;
     const rating = businessData.rating;
     const location = businessData.location;
@@ -98,7 +123,12 @@ export class LocationInputComponent implements OnInit {
     const displayAddress = businessData.location.display_address;
     console.log(displayAddress);
 
-    const displayAddressModified = displayAddress.replace(/\s/g, '+');
+    // Set map marker to business
+    console.log(coordinates);
+    this.lat = coordinates.latitude;
+    this.lng = coordinates.longitude;
+
+    // const displayAddressModified = displayAddress.replace(/\s/g, '+');
     const aliasModified = alias.replace(/-/g, '+');
 
     // const mapURL = `https://www.google.com/maps/search/?api=1&query=${aliasModified}&query=${coordinates.latitude},${coordinates.longitude}`;
@@ -111,6 +141,9 @@ export class LocationInputComponent implements OnInit {
 
     this.mapLink = mapURL;
     this.mapName = 'Route to Meeting Location';
+
+    this.show = true;
+
   }
 
 }
