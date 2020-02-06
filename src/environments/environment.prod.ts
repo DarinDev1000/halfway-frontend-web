f0e5b051cd7b4f5da01d654edab0c9001ba293fc
Darin <darindev1000@gmail.com>
2020-02-05 17:04:38 -0800

    Add new server url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // apiURL: 'https://api.halfwaymeet.tk'
-  apiURL: 'http://localhost:3000'
+  // apiURL: 'http://localhost:3000'
+  apiURL: 'https://api.halfway.darindev1000.com'
 };
